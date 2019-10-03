34f9c65f5cafe41e8f684601a114adbdb5129031
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2019-10-03 10:37:16 -0500

    comenzando con promesas en paralelo

--- clase31-manejoErrores/clase31.js
+++ clase31-manejoErrores/clase31.js
@@ -6,20 +6,26 @@ const opts = {crossDomain: true}
 function obtenerPersonaje(id, callback) {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
 
-    $.get(url, opts, function(persona) {
-        console.log(`Hola, soy ${persona.name}`)
-    
-    if (callback) {
-        callback()
-    }
+    $.get(url, opts, callback).fail(function() {
+        console.log(`Sucedio un error.  No se pudo obtener el personaje ${id}`)
     })
 }
 
-obtenerPersonaje(1, function() {
-    obtenerPersonaje(2, function() {
-        obtenerPersonaje(3, function() {
-            obtenerPersonaje(4, function() {
-                obtenerPersonaje(5)
+obtenerPersonaje(1, function(personaje) {
+    console.log(`Hola, me llamo ${personaje.name}`)
+
+    obtenerPersonaje(2, function(personaje) {
+        console.log(`Hola, me llamo ${personaje.name}`)
+
+        obtenerPersonaje(3, function(personaje) {
+            console.log(`Hola, me llamo ${personaje.name}`)
+
+            obtenerPersonaje(4, function(personaje) {
+                console.log(`Hola, me llamo ${personaje.name}`)
+
+                obtenerPersonaje(5, function(personaje){
+                    console.log(`Hola, soy ${personaje.name}`)
+                })
             })
         })
     })
